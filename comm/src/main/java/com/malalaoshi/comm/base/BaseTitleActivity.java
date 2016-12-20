084ccd248c793fa2802b194917561a7ebd0b31d2
Kang <zhoukang@malalaoshi.com>
2016-12-20 19:00:08 +0800

    PAD-4 add login ui

---
+++ comm/src/main/java/com/malalaoshi/comm/base/BaseTitleActivity.java
@@ -0,0 +1,10 @@
+package com.malalaoshi.comm.base;
+
+import com.malalaoshi.android.core.base.BaseActivity;
+
+/**
+ * Created by kang on 16/12/20.
+ */
+
+public abstract class BaseTitleActivity extends BaseActivity {
+}
--- core/src/main/java/com/malalaoshi/android/core/base/BaseFragment.java
+++ core/src/main/java/com/malalaoshi/android/core/base/BaseFragment.java
@@ -1,10 +1,20 @@
 package com.malalaoshi.android.core.base;
 
-import android.app.Fragment;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.malalaoshi.android.core.R;
 
 /**
  * Created by kang on 16/12/8.
  */
 
 public class BaseFragment extends Fragment {
+
+
 }
